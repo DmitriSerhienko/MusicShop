ae0277d4e285e55e3a44d2a4c48e143c28a9939f
DmitriSerhienko <100857833+DmitriSerhienko@users.noreply.github.com>
2022-11-02 23:29:57 +0200

    update

--- app/src/main/java/com/dimas/musicshop/MainActivity.java
+++ app/src/main/java/com/dimas/musicshop/MainActivity.java
@@ -3,12 +3,47 @@ package com.dimas.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
+    Spinner spinner;
+    ArrayList spinnerArrayList;
+    ArrayAdapter spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        spinnerArrayList = new ArrayList();
+        spinnerArrayList.add("guitar");
+        spinnerArrayList.add("drums");
+        spinnerArrayList.add("keyboard");
+
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
     }
+
+    public void increaseQuantity(View view) {
+        quantity++;
+        TextView tvQuantity = findViewById(R.id.tvQuantity);
+        tvQuantity.setText("" + quantity);
+    }
+
+    public void decreaseQuantity(View view) {
+        quantity--;
+        if (quantity < 0) quantity = 0;
+        TextView tvQuantity = findViewById(R.id.tvQuantity);
+        tvQuantity.setText("" + quantity);
+    }
+
+
+
 }
